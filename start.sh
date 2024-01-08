9e11244f7157b69bc7738c24f8d64578da39bd00
linzhiji <linzhiji@gmail.com>
2024-01-08 17:21:01 +0800

    fix bug

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 . colors.sh
 jobName=app_sport.py
 echo "${YELLOW}check $jobName pid ${NOCOLOR}"
+echo "ps aux | grep "$jobName" | grep -v grep | awk '{print $2}'"
 TAILPID=`ps aux | grep "$jobName" | grep -v grep | awk '{print $2}'`
-
-
 if [[ "0$TAILPID" != "0" ]]; then
     echo "${RED}kill process $TAILPID${NOCOLOR}"
     sudo kill -9 $TAILPID
