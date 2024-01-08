41982e158a016a50dce45448309353e48c93c6d7
linzhiji <linzhiji@gmail.com>
2024-01-08 17:18:32 +0800

    fix bug

--- readme.md
+++ readme.md
@@ -9,10 +9,7 @@ pip install requirements.txt
 
 # 执行
 ```
-# 原始文件拷贝到 srcData 下
-# 文件名在 computeData.py 里修改 inputFileName = "五年级6班"
-# demo.xlsx 有列名和案例数据
-python computeData.py
+python app_sport.py
 ```
 
 # 标准
---
+++ start.sh
@@ -0,0 +1,14 @@
+. colors.sh
+jobName=app_sport.py
+echo "${YELLOW}check $jobName pid ${NOCOLOR}"
+TAILPID=`ps aux | grep "$jobName" | grep -v grep | awk '{print $2}'`
+
+
+if [[ "0$TAILPID" != "0" ]]; then
+    echo "${RED}kill process $TAILPID${NOCOLOR}"
+    sudo kill -9 $TAILPID
+fi
+
+echo "${RED}python $jobName ${NOCOLOR}"
+
+nohup python $jobName > /dev/null 2>&1 &
